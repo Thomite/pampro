4af54b4087a9bef418d64b95942d88e1d666e3ed
thomite@gmail.com <thomite@gmail.com>
2014-04-13 16:56:38 +0100

    Structured module properly, added initial Time_Series and Channel objects

---
+++ pampropy/example_usage.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange
+from datetime import datetime, date, time, timedelta
+import random
+
+import Time_Series
+import Channel
+
+ts = Time_Series.Time_Series()
+
+
+# Create channel A
+
+dt = datetime.strptime("15-Mar-2014 16:32", "%d-%b-%Y %H:%M")
+epoch_length = timedelta(minutes=1)
+
+timestamp_list = []
+for i in range(0,1000):
+	timestamp_list.append(dt)
+	dt = dt + epoch_length
+
+timestamps_a = np.array(timestamp_list)
+
+channel_a = Channel.Channel("A")
+channel_a.set_contents([350+random.random()*2+np.cos(x*0.02)*30 for x in range(0,1000)], timestamps_a)
+
+# Create channel B
+
+dt = datetime.strptime("15-Mar-2014 21:00", "%d-%b-%Y %H:%M")
+epoch_length = timedelta(seconds=30)
+
+timestamp_list = []
+for i in range(0,3000):
+	timestamp_list.append(dt)
+	dt = dt + epoch_length
+
+timestamps_b = np.array(timestamp_list)
+
+channel_b = Channel.Channel("B")
+channel_b.set_contents([500+random.random()+np.sin(x*0.025)*10 for x in range(0,3000)], timestamps_b)
+
+# Create channel C
+
+dt = datetime.strptime("15-Mar-2014 18:03", "%d-%b-%Y %H:%M")
+epoch_length = timedelta(minutes=2, seconds=30)
+
+timestamp_list = []
+for i in range(0,900):
+	timestamp_list.append(dt)
+	dt = dt + epoch_length
+
+timestamps_c = np.array(timestamp_list)
+
+channel_c = Channel.Channel("C")
+channel_c.set_contents([420+random.random()*2+np.sin(x*0.025)*10 for x in range(0,900)], timestamps_c)
+
+#ts.add_channel(channel_a)
+#ts.add_channel(channel_b)
+#ts.add_channel(channel_c)
+
+
+chans = Channel.load_channels("V:/Projects/pampropy/data/ARBOTW.txt", "Actiheart")
+
+for chan in chans:
+	chan.normalise(0,100)
+	ts.add_channel(chan)
+
+ts.draw_separate()
---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pampropy",
+    packages = ["pampropy"],
+    version = "0.0.1",
+    author = "Tom White",
+    author_email = "thomite@gmail.com",
+    description = ("Physical Activity Monitoring Processing in Python"),
+    url = "https://github.com/Thomite/pampropy"
+)
+
