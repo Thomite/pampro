aa72e4229085bf9e18fbaa8b8661b34be592a28a
Tom White <thomite@gmail.com>
2014-05-13 16:32:48 +0100

    Bout intersections

--- examples/example_usage.py
+++ examples/example_usage.py
@@ -9,7 +9,7 @@ import copy
 
 
 
-from pampropy import Time_Series, Channel, Annotation, channel_inference
+from pampropy import Time_Series, Channel, Annotation, channel_inference, Bout
 
 execution_start = datetime.now()
 
@@ -40,8 +40,8 @@ ts.add_channel(awake_probability)
 
 
 # Define a period of interest
-start = datetime.strptime("17-Mar-2014 00:00", "%d-%b-%Y %H:%M")
-end = start + timedelta(days=3)
+start = datetime.strptime("15-Mar-2014 00:00", "%d-%b-%Y %H:%M")
+end = start + timedelta(hours=12)
 
 ts_output = Time_Series.Time_Series("Actiheart output")
 # Save some stats about the time series to a file
@@ -50,12 +50,12 @@ result_chans = activity.piecewise_statistics( timedelta(minutes=10), statistics=
 ts_output.add_channels(result_chans)
 result_chans = ecg.piecewise_statistics( timedelta(minutes=10), statistics=stats )
 ts_output.add_channels(result_chans)
-ts_output.write_channels_to_file(file_target=os.path.join(os.path.dirname(__file__), '..', 'data/ah_data_10m.csv'))
+#ts_output.write_channels_to_file(file_target=os.path.join(os.path.dirname(__file__), '..', 'data/ah_data_10m.csv'))
 
 result_chans = activity.summary_statistics( statistics=stats )
 ts_summary = Time_Series.Time_Series("Actiheart summary")
 ts_summary.add_channels(result_chans)
-ts_summary.write_channels_to_file(file_target=os.path.join(os.path.dirname(__file__), '..', 'data/ah_data_summary.csv'))
+#ts_summary.write_channels_to_file(file_target=os.path.join(os.path.dirname(__file__), '..', 'data/ah_data_summary.csv'))
 
 # Infer vector magnitude from three channels
 #vm = channel_inference.infer_vector_magnitude(awake_probability, ecg, activity)
@@ -63,12 +63,22 @@ ts_summary.write_channels_to_file(file_target=os.path.join(os.path.dirname(__fil
 
 # Get a list of bouts where awake probability was >= 0 and <= 0.001 for 240 epochs or more
 bouts = awake_probability.bouts(0,0.001,240)
+inverted = Bout.time_period_minus_bouts([ts.earliest, ts.latest], bouts)
+
+ecg_low = ecg.bouts(0,80,20)
+intersection = Bout.bout_list_intersection(ecg_low, bouts)
+
+
 awake_probability.add_annotations(bouts)
+ecg_ma.add_annotations(ecg_low)
+activity_ma.add_annotations(bouts)
+ts.get_channel("AH_Activity_td_ma").add_annotations(intersection)
+
 #for bout in bouts:
 # print bout[0].day, bout[0], " -- ", bout[1]
 
-activity_channels = activity.build_statistics_channels(bouts, stats)
-ecg_channels = ecg.build_statistics_channels(bouts, stats)
+activity_channels = activity.build_statistics_channels(inverted, stats)
+ecg_channels = ecg.build_statistics_channels(inverted, stats)
 ts_test = Time_Series.Time_Series("Bouts")
 ts_test.add_channels(activity_channels)
 ts_test.add_channels(ecg_channels)
@@ -86,7 +96,7 @@ awake_probability.draw_properties = {'alpha':1, 'lw':2, 'color':[0.78431,0.196,0
 #for row in dataset:
 #print row
 
-ts_test.draw_separate()
+ts.draw_separate(time_period=[start,end])
 
 execution_end = datetime.now()
 print("{} to {} = {}".format( execution_start, execution_end, execution_end - execution_start))
--- pampropy/Bout.py
+++ pampropy/Bout.py
@@ -1,3 +1,4 @@
+import copy
 
 class Bout(object):
 
@@ -8,16 +9,66 @@ class Bout(object):
 		self.end_timestamp = end_timestamp
 		self.draw_properties = {'lw':0, 'alpha':0.8, 'facecolor':[0.78431,0.78431,0.78431]}
 
+	def contains(self, timepoint):
+
+		return timepoint >= self.start_timestamp and timepoint <= self.end_timestamp
+
+	def overlaps(self, other):
+		
+		return self.contains(other.start_timestamp) or self.contains(other.end_timestamp) or other.contains(self.start_timestamp) or other.contains(self.end_timestamp)
+
+	def intersection(self, other):
+
+		return Bout(max(self.start_timestamp, other.start_timestamp), min(self.end_timestamp, other.end_timestamp))
+
 
 
 def bout_list_intersection(bouts_a, bouts_b):
 
-	# TODO: Return the intersection of two bout lists
+	intersection = []
 
-	return -1
+	for bout_a in bouts_a:
+		for bout_b in bouts_b:
+
+			if bout_a.overlaps(bout_b):
+
+				bout_c = bout_a.intersection(bout_b)
+				#print bout_a.start_timestamp, bout_a.end_timestamp, "overlaps", bout_b.start_timestamp, bout_b.end_timestamp, "=", bout_c.start_timestamp, bout_c.end_timestamp
+				intersection.append(bout_c)
+		
+	#print "Bouts a", len(bouts_a)
+	#print "Bouts b", len(bouts_b)
+	#print "Intersections", len(intersection)
+
+	#for i in intersection:
+	#	i.draw_properties = {"lw":1, "facecolor":[1.0,0.2,0.2], "alpha":0.38}
+
+	return intersection
 
 def bout_list_union(bouts_a, bouts_b):
 
 	# TODO: Return the union of two bout lists
 
 	return -1
+
+def time_period_minus_bouts(time_period, bouts):
+
+	# Currently assumes bouts are sorted in chronological order
+	# Add sort in here for safety?
+
+	results = []
+
+	start = time_period[0]
+
+	for bout in bouts:
+
+		results.append(Bout(start, bout.start_timestamp))
+		start = bout.end_timestamp
+
+	results.append(Bout(start, time_period[1]))
+
+	#for r in results:
+	#	r.draw_properties = {'lw':0, "alpha":0.75, "facecolor":[0.95,0.1,0.1]}
+
+	return results
+	
