2574c5761f6db85ddf9ec5f088ccd10a344bb7e6
Tom White <thomite@gmail.com>
2014-11-12 15:45:35 +0000

    Added python script to append batch results

---
+++ meta/consolidate_results.py
@@ -0,0 +1,36 @@
+'''
+This is a simple script to consolidate results from batch runs into 1 file.
+
+Usage:
+py consolidate_results.py "/path/to/results" "daily"
+This will append together all results with the daily prefix into 1 file called daily_all.csv
+'''
+
+import glob
+import sys
+
+folder = sys.argv[1]
+prefix = sys.argv[2]
+
+# List all matching files
+files = glob.glob(folder + "/" + prefix + "*.csv")
+
+# Create the output file
+output = open(folder + "/" + prefix + "_all.csv", "w")
+
+# Copy all of the data from the first file, including header
+first_file = open(files[0], "r")
+output.write(first_file.read())
+first_file.close()
+
+for filename in files:
+
+	# Copy the data of every other file, excluding the header
+	fh = open(filename, "r")
+	fh.readline()
+	data = fh.read()
+	fh.close()
+
+	output.write(data)
+
+output.close()
