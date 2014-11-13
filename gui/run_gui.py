0997c1e84760882a821005629b36d6c7763340b7
Tom White <thomite@gmail.com>
2014-11-13 16:30:12 +0000

    Updated GUI

--- gui/run_gui.py
+++ gui/run_gui.py
@@ -16,6 +16,7 @@ pampro_lightgrey = "#FAFAFA"
 pampro_background = "#FFFFFF"
 
 all_style = """
+
 /*QProgressBar { border: 1px solid """+pampro_darkgrey+"""; background-color:#FFFFFF; }
 QProgressBar::chunk { background-color: """+pampro_red+"""; width: 1px; }*/
 
@@ -27,6 +28,10 @@ QMainWindow { background-color: """+pampro_background+"""}
 QMenuBar {font-size:14px; background-color:"""+pampro_background+"""; border-bottom:1px solid #000000;}
 QMenuBar::item { padding-right:50px; padding-left:5px; background: transparent; }
 QMenuBar::item::selected {background-color: """+pampro_red+"""; }
+
+QLineEdit {padding:5px; border:1px solid"""+pampro_darkgrey+"""; height:45px;}
+QPushButton {padding:5px; border:1px solid"""+pampro_darkgrey+"""; height:45px;}
+QSpinBox {padding:5px; border:1px solid"""+pampro_darkgrey+"""; height:45px;}
 """
 
 def tohex(r,g,b):
@@ -43,18 +48,19 @@ class GUI(QtGui.QMainWindow):
 
 		super(GUI, self).__init__()
 
-		self.setGeometry(0, 0, 310, 170)
+		self.setGeometry(0, 0, 325, 170)
 		self.setWindowTitle('PAMPRO')
 		self.setWindowIcon(QtGui.QIcon('temp.png'))
 
 		grid = QtGui.QGridLayout()
-		grid.setSpacing(2)
+		grid.setSpacing(10)
 
+		self.file_dialog = QtGui.QFileDialog(self)
 
-		self.button1 = QtGui.QPushButton('Browse', self)
+		self.button1 = QtGui.QPushButton('...', self)
 		grid.addWidget(self.button1, 1, 3, 1, 1)
 
-		self.button2 = QtGui.QPushButton('Browse', self)
+		self.button2 = QtGui.QPushButton('...', self)
 		grid.addWidget(self.button2, 2, 3, 1, 1)
 
 		self.button3 = QtGui.QPushButton('Run', self)
@@ -62,6 +68,7 @@ class GUI(QtGui.QMainWindow):
 
 		self.label1 = QtGui.QLabel(self)
 		self.label1.setText("Job file:")
+		
 		grid.addWidget(self.label1, 1, 1, 1, 1)
 	
 		self.label2 = QtGui.QLabel(self)
@@ -78,8 +85,15 @@ class GUI(QtGui.QMainWindow):
 		self.edit2 = QtGui.QLineEdit(self)
 		grid.addWidget(self.edit2, 2, 2, 1, 1)
 
-		self.edit3 = QtGui.QLineEdit(self)
+		self.edit3 = QtGui.QSpinBox(self)
+		self.edit3.setValue(1)
+		self.edit3.setRange(1, 100000)
 		grid.addWidget(self.edit3, 3, 2, 1, 1)
+		
+
+		self.edit1.setToolTip('Select the job file containing details of all files to be processed.')
+		self.edit2.setToolTip('Select the script to be executed for each file.')
+		self.edit3.setToolTip('Select the number of processes to perform this job.')
 
 		# Actions
 		action1 = QtGui.QAction(QtGui.QIcon('Capture.png'), 'Action1', self)
@@ -94,7 +108,12 @@ class GUI(QtGui.QMainWindow):
 		run_action.setShortcut('Ctrl+R')
 		run_action.triggered.connect(self.run)
 
+		jobfile_action = QtGui.QAction(QtGui.QIcon('exit24.png'), 'blah', self)
+		#jobfile_action.setShortcut('Ctrl+R')
+		jobfile_action.triggered.connect(self.get_job_file)
 
+		self.button1.clicked.connect(self.get_job_file)
+		self.button2.clicked.connect(self.get_script)
 		self.button3.clicked.connect(self.run)
 
 		holder = QtGui.QWidget(self)
@@ -121,25 +140,37 @@ class GUI(QtGui.QMainWindow):
 
 		pass
 
+	def get_job_file(self):
+		filename = self.file_dialog.getOpenFileName()
+		self.edit1.setText(filename)
+
+
+	def get_script(self):
+		filename = self.file_dialog.getOpenFileName()
+		self.edit2.setText(filename)
+
 	def run(self):
 
 		job_file = self.edit1.displayText()
 		script = self.edit2.displayText()
-		num_jobs = int(self.edit3.displayText())
+		num_jobs = int(self.edit3.cleanText())
 
 		if num_jobs > 1:
 			print "Batch job"
 
 			for i in range(1, num_jobs+1):
-				command = "ipython " + script + " " + job_file + " " + str(i) + " " + str(num_jobs)
+				command = "sge ~/anaconda/bin/ipython --matplotlib=None " + script + " " + job_file + " " + str(i) + " " + str(num_jobs)
 				print command
+				#os.system(command)
 		else:
 			print "Single process"
 
-			command = "ipython " + script + " " + job_file + " 1 1"
+			command = "sge ~/anaconda/bin/ipython --matplotlib=None " + script + " " + job_file + " 1 1"
 			print command
-			#os.system("echo 'hello world' >> /pa/data/Tom/pampro/gui/blah.txt")
+			#os.system(command)
+		
 
+		
 
 
 if __name__ == '__main__':
