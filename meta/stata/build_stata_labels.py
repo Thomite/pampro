be761398f132b6b52724892a84e36eb02207492d
Tom White <thomite@gmail.com>
2015-05-13 15:22:13 +0100

    Changed automatic Stata dataset labelling script.

--- meta/stata/build_stata_labels.py
+++ meta/stata/build_stata_labels.py
@@ -1,16 +1,20 @@
 import numpy as np
+import sys
 
 # This quick script generates a Stata do-file to label a PAMPRO dataset using the data dictionary
 
-dd_file = "Z:/data/EPIC3/_results/data_dictionary_1.csv"
-do_file = dd_file.replace("data_dictionary", "do_file")
+dd_file = sys.argv[1]
+
+do_file = dd_file.replace("data_dictionary", "label_variables")
 do_file = do_file.replace(".csv", ".do")
 
-data = np.loadtxt("Z:/data/EPIC3/_results/data_dictionary_1.csv", delimiter=",", skiprows=1, dtype=str, unpack=True)
-output = file(do_file, "w")
+print("Creating " + do_file + " from " + dd_file)
+
+data = np.loadtxt(dd_file, delimiter=",", skiprows=1, dtype="S", unpack=True).astype("U")
+output = open(do_file, "w")
 
 for variable,description in zip(data[0,], data[1,]):
     #print("label variable {} \"{}\"".format(variable, description))
-    output.write(("label variable {} \"{}\" \n".format(variable, description))
+    output.write(("label variable {} \"{}\" \n".format(variable, description)))
 
 output.close()
