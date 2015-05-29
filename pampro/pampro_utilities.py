4596cf73d5bac0f0d3b672f4963f27924640c14b
Tom White <thomite@gmail.com>
2015-05-29 10:40:04 +0100

    Updated data dictionary to account for stats changes.

--- pampro/pampro_utilities.py
+++ pampro/pampro_utilities.py
@@ -57,53 +57,65 @@ def design_file_header(statistics):
     #print(file_header)
     return file_header
 
+def design_variable_descriptions(signal_name, stat):
+
+    variable_descriptions = []
+
+    if stat[0] == "generic":
+
+        for stat1 in stat[1]:
+            if stat1 == "mean":
+                variable_descriptions.append("Mean average {}".format(signal_name))
+            elif stat1 == "sum":
+                variable_descriptions.append("Sum {}".format(signal_name))
+            elif stat1 == "std":
+                variable_descriptions.append("Standard deviation {}".format(signal_name))
+            elif stat1 == "min":
+                variable_descriptions.append("Lowest {}".format(signal_name))
+            elif stat1 == "max":
+                variable_descriptions.append("Highest {}".format(signal_name))
+            elif stat1 == "n":
+                variable_descriptions.append("Number of observations in {}".format(signal_name))
+
+    elif stat[0] == "cutpoints":
+        for low,high in stat[1]:
+
+            variable_descriptions.append("Number of values in {} at >= {} & <= {}".format(signal_name, low, high))
+
+    elif stat[0] == "bigrams":
+
+        for val1 in stat[1]:
+            for val2 in stat[1]:
+                variable_descriptions.append("Number of transitions from {} to {}".format(val1, val2))
+
+    elif stat[0] == "percentiles":
+
+        for percentile in stat[1]:
+
+            suffix = "th"
+            if str(percentile)[-1] == "3":
+                suffix = "rd"
+            elif str(percentile)[-1] == "1":
+                suffix = "st"
+            elif str(percentile)[-1] == "2":
+                suffix = "nd"
+            variable_descriptions.append("{}{} percentile of {}".format(percentile, suffix, signal_name))
+
+    return variable_descriptions
+
 def design_data_dictionary(statistics_dictionary):
 
     data_dictionary = collections.OrderedDict()
 
     for channel, statistics in statistics_dictionary.items():
 
-
         for stat in statistics:
 
             variable_names = design_variable_names(channel, stat)
-            for variable_name, stat1 in zip(variable_names, stat[1]):
-                if stat[0] == "generic":
-                    if stat1 == "mean":
-                        data_dictionary[variable_name] = "Mean average of {}".format(channel)
-                    elif stat1 == "sum":
-                        data_dictionary[variable_name] = "Sum of values in {}".format(channel)
-                    elif stat1 == "std":
-                        data_dictionary[variable_name] = "Standard deviation of {}".format(channel)
-                    elif stat1 == "min":
-                        data_dictionary[variable_name] = "Lowest value in {}".format(channel)
-                    elif stat1 == "max":
-                        data_dictionary[variable_name] = "Highest value in {}".format(channel)
-                    elif stat1 == "n":
-                        data_dictionary[variable_name] = "Number of observations in {}".format(channel)
+            variable_descriptions = design_variable_descriptions(channel, stat)
 
-                elif stat[0] == "cutpoints":
+            for variable_name, variable_description in zip(variable_names, variable_descriptions):
+                data_dictionary[variable_name] = variable_description
 
-                    for low,high in stat1:
-                        data_dictionary[variable_name] = "Number of values in {} at >= {} & <= {}".format(channel, low, high)
-
-                elif stat[0] == "bigrams":
-
-                    for val1 in stat1:
-                        for val2 in stat1:
-                            data_dictionary[variable_name] = "Number of transitions from {} to {}".format(val1, val2)
-
-                elif stat[0] == "percentiles":
-
-                    for percentile in stat1:
-
-                        suffix = "th"
-                        if str(percentile)[-1] == "3":
-                            suffix = "rd"
-                        elif str(percentile)[-1] == "1":
-                            suffix = "st"
-                        elif str(percentile)[-1] == "2":
-                            suffix = "nd"
-                        data_dictionary[variable_name] = "{}{} percentile of {}".format(percentile, suffix, channel)
 
     return data_dictionary
