9330dc017338493b6968b8c90705382ee5a9180a
Tom White <thomite@gmail.com>
2015-10-19 16:37:12 +0100

    Nonwear sensitivity bug fixed

--- pampro/channel_inference.py
+++ pampro/channel_inference.py
@@ -272,7 +272,7 @@ def infer_nonwear_triaxial(x, y, z, minimum_length=timedelta(hours=1), noise_cut
 
     ''' Use the 3 channels of triaxial acceleration to infer periods of nonwear '''
 
-    x_intersect_y_intersect_z = infer_still_bouts_triaxial(x,y,z, approximate_timestamps=x.sparsely_timestamped)
+    x_intersect_y_intersect_z = infer_still_bouts_triaxial(x,y,z, noise_cutoff_mg=noise_cutoff_mg, approximate_timestamps=x.sparsely_timestamped)
 
     Bout.cache_lengths(x_intersect_y_intersect_z)
 
